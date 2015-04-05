ea813f224a076eef12f5d6a3c5d1d9cdffdbc5b6
James English <james.t.english@gmail.com>
2015-04-05 16:52:45 -0500

    setting up front end

--- PyDog.py
+++ PyDog.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext import restful
 from api.dogs import DogsList
 from api.jobs import DogScrape
+
 
 app = Flask(__name__)
 api = restful.Api(app)
@@ -9,6 +10,10 @@ api = restful.Api(app)
 api.add_resource(DogsList, '/dogs/')
 api.add_resource(DogScrape, '/jobs/dogscrape/<job_id>/')
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
